d34330f7315e97eeb226d69cb699fe128595597d
Nownew2711 <abinaya.radhakrishnan91@gmail.com>
2024-08-17 17:01:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -108,9 +108,6 @@ iface = gr.Interface(
     outputs=[gr.Textbox(), gr.File(label="Download CSV")]
 )
 
-iface.launch()
-
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get('PORT', 5000))
     iface.launch(server_name="0.0.0.0", server_port=port)
